381f0ca10c0954f0b48d841f90329c600cde3a7d
unknown <hadil.a.hassouna@gmail.com>
2019-10-29 00:55:57 -0700

    Tic Tac Toe

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,70 @@
 
 
+const ticTacToeGame = new TicTacToeGame();
+ticTacToeGame.start();
+
+function TicTacToeGame() {
+    const board = new Board();
+    const humanPlayer = new HumanPlayer(board);
+    const computerPlayer = new ComputerPlayer(board);
+    let turn = 0;
+
+    this.start = function () {
+        const config = { childList: true };
+        cont observer = new MutationObserver(() => takeTurn());
+        board.positions.forEach((el) => observer.observe(el, config));
+        takeTurn();
+    }
+    function takeTurn() {
+
+        if (turn % 2 === 0) {
+            humanPlayer.takeTurn();
+        } else {
+            computerPlayer.takeTurn();
+        }
+        turn ++;
+    };
+
+    }
+}
+
+function Board () {
+    this.positions = Array.from(document.querySelectorAll('.col'));
+
+
+}
+
+function HumanPlayer(board) {
+    this.takeTurn = function() {
+        board.positions.forEach (el => ell.addEventListener('click', handleTurnTaken));
+    }
+
+    function handleTurnTaken(event) {
+        event.target.innerText = 'x';
+    }
+
+    
+}
+
+function ComputerPlayer(board) {
+    this.takeTurn = function() {
+        console.log("computer player turn")
+    }
+    
+}
+// document.addEventListener("click", function(){
+//     // document.getElementById("x").innerHTML = "x";
+//     var x = document.getElementsByClassName("container");
+    
+// })
+// ticTacToeGame.start();
+// function TicTacToeGame() {
+//     const board = new board();
+
+//     this.start = function() {
+
+
+//     }
+// }
+
+
